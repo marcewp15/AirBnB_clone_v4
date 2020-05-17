ff0965473fbba8e566e9da1e0bebaef8ffe7bac7
lucho1503 <lmmc.1503@gmail.com>
2020-05-17 21:01:11 +0000

    1.

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+$(document).ready(function () {
+  const $checkboxes = $("input[type='checkbox']")
+  const amenId = [];
+  const amenName = [];
+  $checkboxes.change(function () {
+    if (this.checked) {
+      amenId.push($(this).attr('data-id'));
+      amenName.push($(this).attr('data-name'));
+      $('.amenities h4').text(amenName);
+    } else {
+      const i = amenId.indexOf($(this).attr('data-id'));
+      amenId.splice(i, 1);
+      amenName.splice(i, 1);
+      if (amenName.length > 0) {
+        $('.amenities h4').text(amenName);
+      } else {
+        $('.amenities h4').html('&nbsp;');
+      }
+    }
+  });
+});
