dea2b4ad41df457700b4c927235a3c22a9b9ffff
marcewp15 <970@holbertonschool.com>
2020-05-18 15:12:00 -0500

    [Feature] Update the status of the API before requesting
    Files:
    2-hbnb.py
    static/scripts/2-hbnb.js
    static/styles/3-header.css
    templates/2-hbnb.html

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+$(document).ready(function () {
+  const $checkboxes = $("input[type='checkbox']")
+  const amenId = [];
+  const amenName = [];
+  $checkboxes.change(function () {
+    if (this.checked) {
+      amenId.push($(this).attr('data-id'));
+      amenName.push($(this).attr('data-name'));
+      $('.amenities h4').text(amenName);
+    } else {
+      const i = amenId.indexOf($(this).attr('data-id'));
+      amenId.splice(i, 1);
+      amenName.splice(i, 1);
+      if (amenName.length > 0) {
+        $('.amenities h4').text(amenName);
+      } else {
+        $('.amenities h4').html('&nbsp;');
+      }
+    }
+  });
+});
+
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+  if (data.status == 'OK') {
+    $('DIV#api_status').addClass('available');
+  } else {
+    $('DIV#api_status').removeClass('available');
+  }
+});
